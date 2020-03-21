aeccb36fdc12ce482c03f3b79c77a4e5f349986f
Tran Son Hoang <hayden.tran.d@gmail.com>
2020-03-21 22:53:55 +0800

    db - implement migration

--- migrations/20200316225153_app.js
+++ migrations/20200316225153_app.js
@@ -1,8 +1,33 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('users', function (table) {
+        table.increments();
+        table.string('first_name');
+        table.string('last_name');
+        table.string('username');
+        table.string('email');
+        table.string('password');
+        table.string('avatar_url');
+        table.text('bio').nullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.string('role').references('roles.role_id');
+        table.string('status').references('user_status.user_status_id');
+        table.unique('email');
+        table.unique('username');
+    }).createTable('roles', function (table) {
+        table.string('role_id').primary();
+        table.string('name');
+        table.string('desc').nullable()
+    }).createTable('user_status', function (table) {
+        table.string('user_status_id').primary();
+        table.string('name');
+        table.string('desc').nullable()
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+    .dropTable("users")
+    .dropTable("roles")
+    .dropTable("user_status")
 };
